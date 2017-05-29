38e106d73c8a36cce518f9a3a835bd01d9202294
Masood Sadat <masood.sdt@gmail.com>
2017-05-29 13:26:19 +0200

    updates
    last updates for today

--- Nielsen Data Cleaning Script.R
+++ Nielsen Data Cleaning Script.R
@@ -34,7 +34,7 @@ for (i in weeks) {
 }
 dup <- which(duplicated(brands$brand_id))
 brands <- brands[-dup,]
-write.csv(brands, "brands.master.csv", row.names = FALSE)
+#write.csv(brands, "brands.master.csv", row.names = FALSE)
 
 
 # categories
@@ -46,7 +46,7 @@ for (i in weeks) {
 }
 dup <- which(duplicated(categories$category_id))
 categories <- categories[-dup,]
-write.csv(categories, "categories.master.csv", row.names = FALSE)
+#write.csv(categories, "categories.master.csv", row.names = FALSE)
 
 
 # manufacturers
@@ -58,7 +58,7 @@ for (i in weeks) {
 }
 dup <- which(duplicated(manufacturers$manufacturer_id))
 manufacturers <- manufacturers[-dup,]
-write.csv(manufacturers, "manufacturers.master.csv", row.names = FALSE)
+#write.csv(manufacturers, "manufacturers.master.csv", row.names = FALSE)
 
 
 # products
@@ -70,7 +70,7 @@ for (i in weeks) {
 }
 dup <- which(duplicated(products$product_id))
 products <- products[-dup,]
-write.csv(products, "products.master.csv", row.names = FALSE)
+#write.csv(products, "products.master.csv", row.names = FALSE)
 
 
 # sales
@@ -86,6 +86,10 @@ sales$sales <- as.numeric(sub(",", ".", sales$sales))
 sales$volume <- round(sales$sales / sales$price)
 sales <- as.data.table(sales)
 sales[, "promo" := NULL]
+
+
+# stores
+stores <- fread("stores.csv")
 
 
 # qbrik
@@ -119,7 +123,7 @@ imdb <- read.dbf("SCT data/IMDB.DBF")
 weird_price <- sales[, .(max = max(price), min = min(price), uniq.week = n_distinct(period_id)), by = product_id] %>% 
   .[max > 5*min] %>% merge(products, by = "product_id") %>% select(c(1, 5, 2:4))
 
-write.csv(weird_price, file = "#Results tables/products with weird prices - max(price) > min(price)*5.csv")
+#write.csv(weird_price, file = "#Results tables/products with weird prices - max(price) > min(price)*5.csv")
 
 weird_price <- weird_price[, product_id]
 sales <- sales[!product_id %in% weird_price] %>% as.data.table()
@@ -127,17 +131,17 @@ rm(weird_price)
 
 
 # Sanity check 2: products
-weird_products <- sales[, .(uniq.week = n_distinct(period_id), uniq.store = n_distinct(store_id)), by = .(product_id)] %>% 
-  .[uniq.week == 1 & uniq.store == 1] %>% merge(products, by = "product_id") %>% select(c(1,4,2,3))
-# are these from one store? one period?
+weird_products <- sales[, .(uniq.week = n_distinct(period_id), uniq.store = n_distinct(store_id)), by = .(product_id, store_id)] %>% 
+  .[uniq.week == 1 & uniq.store == 1] %>% merge(products, by = "product_id") %>% select(c(2,1,5,3,4))
+# Q: are these from one store? one period? A: No, it comes from all stores without apparent pattern
 write.csv(weird_products, file = "#Results tables/products sold only in one store and one week - weird?.csv")
 weird_products <- weird_products[, product_id]
 sales <- sales[!product_id == weird_products] %>% as.data.table()
 
 
 # Sanity check 3: Dummy Bucket Product
-sales <- merge(sales, products, by = "product_id") %>% select(c(1,10,2:9)) %>% .[!product_name == "Dummy Bucket Product"] %>% as.data.table()
+sales <- merge(sales, products, by = "product_id") %>% select(c(1,10,2:9)) %>% .[product_name == "Dummy Bucket Product"] %>% as.data.table()
 write.csv(sales, file = "sales.master.csv", row.names = FALSE)
-
+merge(sales, products, by = "product_id") %>% select(c(1,10,2:9)) %>% .[product_name == "Dummy Bucket Product"] %>% write.csv(file = "#Results tables/dummy bucket prodct.csv")
 
 # Sanity check 4: 
